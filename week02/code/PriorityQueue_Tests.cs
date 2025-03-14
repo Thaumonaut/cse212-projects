d54cc7ea6eee492b3bbac58da0480dd179cc871c
Jacob Perry <perryjay123@live.com>
2025-03-14 08:48:58 -0700

    W02 - Code

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,24 +6,67 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: When items are added to the queue they are placed at that back.
+    // Expected Result: items are placed in queue in the order they were called.
+    // Defect(s) Found: None 😎
     public void TestPriorityQueue_1()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("item1", 1);
+        priorityQueue.Enqueue("item2", 2);
+        Console.WriteLine(priorityQueue);
+        var expected = "[item1 (Pri:1), item2 (Pri:2)]";
+        Assert.AreEqual(priorityQueue.ToString(), expected);
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Add multiple items to queue with unique priority and dequeue the item with the highest priority.
+    // Expected Result: The value of the item with the highest priority is returned when the dequeue function is called
+    // Defect(s) Found: none
     public void TestPriorityQueue_2()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+
+        priorityQueue.Enqueue("item_1", 5);
+        priorityQueue.Enqueue("item_2", 3);
+        priorityQueue.Enqueue("item_3", 8);
+        priorityQueue.Enqueue("item_4", 3);
+
+        var highest = priorityQueue.Dequeue();
+        var expected = "item_3";
+
+        Assert.AreEqual(highest, expected);
     }
 
     // Add more test cases as needed below.
+
+    [TestMethod]
+    // Scenario: Add multiple items to queue with a pair of the same priority and dequeue to get the highest priority.
+    // Expected Result: The value of the first item with the highest priority is returned when the dequeue function is called
+    // Defect(s) Found: Last item with highest priority was returned.
+    public void TestPriorityQueue_3()
+    {
+        var priorityQueue = new PriorityQueue();
+
+        priorityQueue.Enqueue("item_1", 8);
+        priorityQueue.Enqueue("item_2", 3);
+        priorityQueue.Enqueue("item_3", 8);
+        priorityQueue.Enqueue("item_4", 3);
+
+        var highest = priorityQueue.Dequeue();
+        var expected = "item_1";
+
+        Assert.AreEqual(highest, expected);
+    }
+
+    [TestMethod]
+    // Scenario: Attempts to dequeue when there are no items in the queue
+    // Expected Result: Invalid Operation Exception is called.
+    // Defect(s) Found: none
+    public void TestPriorityQueue_4()
+    {
+        var priorityQueue = new PriorityQueue();
+
+        Assert.ThrowsException<InvalidOperationException>(() => priorityQueue.Dequeue());
+    }
 }
